9141a4817f54d8731c080ff47c5ebf1588ea50de
TR <treerootboy@gmail.com>
2020-09-01 10:24:25 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -70,9 +70,10 @@ EOF
 which composer && composer config -g repo.packagist composer https://mirrors.aliyun.com/composer/
 
 # npm
-echo '\n#alias for cnpm\nalias cnpm="npm --registry=https://registry.npm.taobao.org \
-  --cache=$HOME/.npm/.cache/cnpm \
-  --disturl=https://npm.taobao.org/dist \
+echo '#alias for cnpm
+alias cnpm="npm --registry=https://registry.npm.taobao.org
+  --cache=$HOME/.npm/.cache/cnpm
+  --disturl=https://npm.taobao.org/dist
   --userconfig=$HOME/.cnpmrc"' >> ~/.bashrc && source ~/.bashrc
 
 
