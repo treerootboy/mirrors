7525f6cd6628b541a0168521f9d8d95fee6d35f4
TR <treerootboy@gmail.com>
2020-09-01 10:21:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
-function install-centos-mirror() {
+#!/bin/bash
+install_centos_mirror() {
   curl -o /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-$VERSION_ID.repo
-  
+
   # epel 在 centos 8 有不同的处理
   if [ "$VERSION_ID" -gt 7 ]; then
     yum install -y https://mirrors.aliyun.com/epel/epel-release-latest-8.noarch.rpm
@@ -10,23 +10,21 @@ function install-centos-mirror() {
   else
     curl -o /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-$VERSION_ID.repo
   fi
-  
+
   yum update
 }
 
-function install-ubuntu-mirror() {
+install_ubuntu_mirror() {
   sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list
   apt update
 }
 
-function install-debian-mirror() {
-#TODO
+install_debian_mirror() {
   curl -o /etc/apt/sources.list https://raw.githubusercontent.com/treerootboy/mirrors/master/os/debian/$VERSION_ID
   apt update
 }
 
-function install-alpine-mirror() {
-#TODO
+install_alpine_mirror() {
   sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories
   apk update
 }
@@ -36,35 +34,36 @@ function install-alpine-mirror() {
 ###########
 source /etc/os-release
 case $ID in
-centos):
-  install-centos-mirror
+centos)
+  install_centos_mirror
   ;;
 
-ubuntu):
-  install-ubuntu-mirror
-  ;;
-  
-debian):
-  install-debian-mirror
-  ;;
-  
-alpine):
-  install-alpine-mirror
+ubuntu)
+  install_ubuntu_mirror
   ;;
 
+debian)
+  install_debian_mirror
+  ;;
+
+alpine)
+  install_alpine_mirror
+  ;;
+esac
 
 ###########
 # package #
 ###########
 
 # pip
-# cat <<EOF > ~/pip/pip.conf
-# [global]
-# index-url = https://mirrors.aliyun.com/pypi/simple/
+mkdir ~/pip
+cat <<EOF > ~/pip/pip.conf
+ [global]
+ index-url = https://mirrors.aliyun.com/pypi/simple/
 
-# [install]
-# trusted-host=mirrors.aliyun.com
-# EOF
+ [install]
+ trusted-host=mirrors.aliyun.com
+EOF
 
 # composer
 which composer && composer config -g repo.packagist composer https://mirrors.aliyun.com/composer/
@@ -74,7 +73,7 @@ echo '\n#alias for cnpm\nalias cnpm="npm --registry=https://registry.npm.taobao.
   --cache=$HOME/.npm/.cache/cnpm \
   --disturl=https://npm.taobao.org/dist \
   --userconfig=$HOME/.cnpmrc"' >> ~/.bashrc && source ~/.bashrc
-  
-  
+
+
 # golang
 export GOPROXY=https://mirrors.aliyun.com/goproxy/
